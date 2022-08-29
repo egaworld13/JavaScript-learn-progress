6ce5776e7944dc70593051ffceab38f2a5b8def3
Edgars Roze <edgarsroze13@gmail.com>
2022-08-29 13:58:35 +0300

    arrays methods

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -2,7 +2,7 @@
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
-// BANKIST APP
+// *BANKIST APP
 
 // Data
 const account1 = {
@@ -74,3 +74,41 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+// let arr = ['a', 'b', 'c', 'd', 'e'];
+//* SLICE method - dont modify original array
+// console.log(arr.slice(2));
+// console.log(arr.slice(2, 3)); // 2-3=1
+// console.log(arr.slice(-1)); //always last element of array
+// console.log(arr.slice()); // get full array.
+// console.log([...arr]); // get full array too.
+
+//* SPLICE method modify original array
+// console.log(arr.splice(2)); // modify original array
+// arr.splice(-1);
+// arr.splice(1, 2); // 1  is start point and 2 is amount of values will be taken out.
+// console.log(arr);
+
+// * REVERSE method modify original array!
+// arr = ['a', 'b', 'c', 'd', 'e'];
+// const arr2 = ['j', 'i', 'h', 'g', 'f'];
+// console.log(arr2.reverse());
+// console.log(arr2);
+//* CONCAT
+// const letters = arr.concat(arr2); //method 1
+// console.log(letters);
+// console.log([...arr, ...arr2]); // method 2
+//* JOIN
+// console.log(letters.join(' - '));
+
+//* NEW METHOD -> at
+const arr = [23, 11, 64];
+console.log(arr[0]); // method 1
+console.log(arr.at(0)); // method 2
+//?Get last element of array
+console.log(arr[arr.length - 1]);
+console.log(arr.slice(-1)[0]);
+console.log(arr.at(-1)); // modern JS  the easyest way.
+
+//? work on strings
+console.log('Edgars'.at(0));
+console.log('Edgars'.at(-1));
