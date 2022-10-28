caba08bf1f93f8b7a3e192149c55c861480626d4
Edgars Roze <edgarsroze13@gmail.com>
2022-10-28 14:29:34 +0300

    Promises,await,async func,challanges

--- Challenge/script.js
+++ Challenge/script.js
@@ -759,3 +759,105 @@ if(markBMI<johnBMI){
 // console.log(tesla);
 // tesla.accelerate().accelerate().brake().chargeBattery(50);
 // console.log(tesla);
+//////* ASYNCHRONOUS JavaScript
+//*Coding challenge #1
+
+// const whereAmI = function (lat, lng) {
+//   fetch(`https://geocode.xyz/${lat},${lng}?geoit=JSON`)
+//     .then(response => {
+//       if (!response.ok)
+//         throw new Error(`Problem with geocoding ${response.status}`);
+//       return response.json();
+//     })
+//     .then(data => console.log(`You are in ${data.city}, ${data.country}`))
+//     .catch(err => console.error(`${err.message}📢`));
+// };
+
+// whereAmI(52.508, 13.381);
+// whereAmI(19.037, 72.873);
+// whereAmI(-33.933, 18.474);
+//! Challenge continues at 10-Asychronus JS file.
+
+//*Coding challenge #2
+//? wait function
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+
+//? 1. Create img function
+const imgContainer = document.querySelector('.images');
+
+const createImage = function (imgPath) {
+  return new Promise(function (resolve, reject) {
+    const img = document.createElement('img');
+    img.src = imgPath;
+
+    //? load event listener. call func when img is loaded!
+    //? RESOLVE
+    img.addEventListener('load', function () {
+      imgContainer.append(img);
+      resolve(img);
+    });
+    //? REJECT
+    img.addEventListener('error', function () {
+      reject(new Error('Image not found'));
+    });
+  });
+};
+// let currentImg;
+// createImage('./img/img2.jpg')
+//   .then(img => {
+//     currentImg = img;
+//     console.log('img 1 loaded');
+//     return wait(2);
+//   })
+//   .then(() => {
+//     currentImg.style.display = 'none';
+//     return createImage('./img/img1.jpg');
+//   })
+//   .then(img => {
+//     currentImg = img;
+//     console.log('img 2 loaded');
+//     return wait(2);
+//   })
+//   .then(() => (currentImg.style.display = 'none'))
+//   .catch(err => console.error(err));
+
+//* CODING CHALLENGE #3
+//? PART 1
+const loadNPause = async function () {
+  try {
+    //?Load img 1
+
+    let img = await createImage('./img/img1.jpg');
+    console.log('img 1 loaded');
+    await wait(2);
+    img.style.display = 'none';
+    //?Load img 2
+
+    img = await createImage('./img/img2.jpg');
+    console.log('img 2 loaded');
+    await wait(2);
+    img.style.display = 'none';
+  } catch (err) {
+    console.log(err);
+  }
+};
+// loadNPause();
+
+//? PART 2
+const loadAll = async function (imgArr) {
+  try {
+    const imgs = imgArr.map(async img => await createImage(img));
+    console.log(imgs);
+
+    const imgEl = await Promise.all(imgs);
+    console.log(imgEl);
+    imgEl.forEach(img => img.classList.add('parallel'));
+  } catch (err) {
+    console.log(err);
+  }
+};
+loadAll(['./img/img1.jpg', './img/img2.jpg', './img/img3.jpg']);
